86b65ea4f16698af18588c9fd48dbb7141c12555
Umair Idris <umair.idris@mail.utoronto.ca>
2015-09-10 17:16:55 -0400

    Initial commit

---
+++ app/src/main/java/ca/utoronto/utsc/tracademia/PointsActivityFragment.java
@@ -0,0 +1,22 @@
+package ca.utoronto.utsc.tracademia;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PointsActivityFragment extends Fragment {
+
+    public PointsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_points, container, false);
+    }
+}
