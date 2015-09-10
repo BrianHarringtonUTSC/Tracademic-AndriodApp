e0a96d8366d6ac2bced87ee08b162bdc82033f07
markusfriesen <markusfriesen@bitbucket.org>
2015-09-10 17:45:34 -0400

    Update documentation for BarecodeScanner

--- app/src/main/java/ca/utoronto/utsc/tracademia/BarcodeScanner.java
+++ app/src/main/java/ca/utoronto/utsc/tracademia/BarcodeScanner.java
@@ -1,20 +1,24 @@
 package ca.utoronto.utsc.tracademia;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+/*
+ * This BarcodeScanner class is responsible to open an existing barcode scanning app and
+ * fetch the library number from that result
+ */
 public class BarcodeScanner extends AppCompatActivity implements OnClickListener{
 
     private Button scanBtn;
@@ -80,11 +84,10 @@ public class BarcodeScanner extends AppCompatActivity implements OnClickListener
 
         if (scanningResult != null) {
             //we have a result
-            String scanContent = scanningResult.getContents();
-            String scanFormat = scanningResult.getFormatName();
-
-            formatTxt.setText("FORMAT: " + scanFormat);
+            String libraryNumber = scanningResult.getContents();
             contentTxt.setText("CONTENT: " + scanContent);
+
+            //TODO:: Check if the library number is valid. Then upload the required points.
         }
         else{
             Toast toast = Toast.makeText(getApplicationContext(),
