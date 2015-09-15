8ad339c93604a65774bd380599406a56900df2c6
markusfriesen <markusfriesen@bitbucket.org>
2015-09-15 19:10:28 -0400

    Fixing silly things

--- app/src/main/java/ca/utoronto/utsc/tracademia/AwardPointsActivity.java
+++ app/src/main/java/ca/utoronto/utsc/tracademia/AwardPointsActivity.java
@@ -5,8 +5,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  * Created by markus on 9/15/15.
@@ -15,6 +18,7 @@ public class AwardPointsActivity  extends AppCompatActivity implements OnClickLi
     int num_points = 5;
     String name = "Bob";
     PointType pointType = PointType.Challenge;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +27,39 @@ public class AwardPointsActivity  extends AppCompatActivity implements OnClickLi
 
         Intent intent = getIntent();
         String libraryNumber = intent.getStringExtra(getString(R.string.libraryNumber));
-        
+
+        submitButton = (Button)findViewById(R.id.give_point);
+        submitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_points, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void onClick(View v) {
         if(v.getId()==R.id.give_point){
             new AlertDialog.Builder(this)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setIcon(android.R.drawable.ic_dialog_info)
                     .setTitle(R.string.confirm_title)
-                    .setMessage("You are giving" + name +" "+ num_points + " " + pointType.toString() +"points?")
+                    .setMessage("You are giving " + name +" "+ num_points + " " + pointType.toString() +" points!")
                     .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
