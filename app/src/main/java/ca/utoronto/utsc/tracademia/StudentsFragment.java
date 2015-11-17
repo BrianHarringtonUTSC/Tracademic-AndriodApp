1684217a1474e96581ffd5bffd47dd5447dd7a00
Umair Idris <umair.idris@mail.utoronto.ca>
2015-11-17 01:15:11 -0500

    Add Swipe down to refresh.

--- app/src/main/java/ca/utoronto/utsc/tracademia/StudentsFragment.java
+++ app/src/main/java/ca/utoronto/utsc/tracademia/StudentsFragment.java
@@ -3,6 +3,7 @@ package ca.utoronto.utsc.tracademia;
 import android.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,12 +13,13 @@ import android.view.ViewGroup;
 import com.google.gson.Gson;
 
 
-public class StudentsFragment extends Fragment {
+public class StudentsFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private static final String TAG = "RecyclerViewFragment";
 
     protected RecyclerView mRecyclerView;
     protected RecyclerView.LayoutManager mLayoutManager;
+    protected SwipeRefreshLayout mSwipeLayout;
     protected StudentsAdapter mAdapter;
 
     @Override
@@ -28,7 +30,7 @@ public class StudentsFragment extends Fragment {
 
         mAdapter = ((OnStudentSelectedListener) getActivity()).getStudentsAdapter();
 
-        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
 
         // LinearLayoutManager is used here, this will layout th\e elements in a similar fashion
         // to the way ListView would layout elements. The RecyclerView.LayoutManager defines how
@@ -37,7 +39,10 @@ public class StudentsFragment extends Fragment {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
 
-        GetStudentsRequestTask getStudentsRequestTask = new GetStudentsRequestTask(mAdapter);
+        mSwipeLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe_refresh_container);
+        mSwipeLayout.setOnRefreshListener(this);
+
+        GetStudentsRequestTask getStudentsRequestTask = new GetStudentsRequestTask();
         getStudentsRequestTask.execute(MainActivity.BASE_URL +  "api/users");
 
         return rootView;
@@ -48,25 +53,26 @@ public class StudentsFragment extends Fragment {
         // Save currently selected layout manager.
         super.onSaveInstanceState(savedInstanceState);
     }
-}
-
-class GetStudentsRequestTask extends AsyncTask<String, String, String> {
-
-    private StudentsAdapter mAdapter;
-
-    public GetStudentsRequestTask(StudentsAdapter mAdapter) {
-        this.mAdapter = mAdapter;
-    }
 
     @Override
-    protected String doInBackground(String... params) {
-        return HTTPClient.getWebpage(params[0]);
+    public void onRefresh() {
+        GetStudentsRequestTask getStudentsRequestTask = new GetStudentsRequestTask();
+        getStudentsRequestTask.execute(MainActivity.BASE_URL + "api/users");
     }
 
-    @Override
-    protected void onPostExecute(String result) {
-        Student[] studentArray = new Gson().fromJson(result, Student[].class);
-        mAdapter.getStudents().clear();
-        mAdapter.addItemsToList(studentArray);
+    class GetStudentsRequestTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            return HTTPClient.getWebpage(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Student[] studentArray = new Gson().fromJson(result, Student[].class);
+            mAdapter.getStudents().clear();
+            mAdapter.addItemsToList(studentArray);
+            mSwipeLayout.setRefreshing(false);
+        }
     }
 }
+
